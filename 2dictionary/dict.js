87a4904f47c85af2aba55bb62d79de761761060c
Tan Tsi Ham <143875582+Tan-Tsi-Ham@users.noreply.github.com>
2025-04-04 14:57:20 +0800

    Add files via upload

--- 2dictionary/dict.js
+++ 2dictionary/dict.js
@@ -1,3 +1,4 @@
+
 // 初始化列表
 function initList() {
     // 注意dict.js可能着复制多份，分予每个选项！
@@ -10,21 +11,35 @@ function initList() {
         return response.json(); // 解析 JSON 数据
     })
     .then(dictionaryData => {
-        const list = document.getElementById('entryList');
+        // 两个方案，不知道哪个快一点
+
+        // 方案一：直接设置超链接跳转（可直接复制粘贴为无JavaScript）
+        let htmlContent='';
         Object.keys(dictionaryData).forEach(entry => {
-            const li = document.createElement('li');
-            li.className = 'entry-item';
-            // li.textContent = entry;
-            const text=dictionaryData[entry].text;
-            li.onclick = () => {
-                // 通过URL参数传递条目名称
-                window.open(`../item.html?word=${encodeURIComponent(entry)}`,"_blank");
-            };
-            const span = document.createElement('span');
-            span.textContent = `${text}`;
-            li.appendChild(span);
-            list.appendChild(li);
+            htmlContent+=`
+                <a href="../item.html?word=${encodeURIComponent(entry)}">
+                   <li class="entry-item"> ${dictionaryData[entry].text}</li>
+                </a>
+            `;
         });
+        document.getElementById('entryList').innerHTML = htmlContent;
+
+        // 方案二：用JavaScript执行跳转
+        // const list = document.getElementById('entryList');
+        // Object.keys(dictionaryData).forEach(entry => {
+        //     const li = document.createElement('li');
+        //     li.className = 'entry-item';
+        //     // li.textContent = entry;
+        //     const text=dictionaryData[entry].text;
+        //     li.onclick = () => {
+        //         // 通过URL参数传递条目名称
+        //         window.open(`../item.html?word=${encodeURIComponent(entry)}`,"_blank");
+        //     };
+        //     const span = document.createElement('span');
+        //     span.textContent = `${text}`;
+        //     li.appendChild(span);
+        //     list.appendChild(li);
+        // });
     })
     .catch(error => {
         console.error('There has been a problem with your fetch operation:', error);
